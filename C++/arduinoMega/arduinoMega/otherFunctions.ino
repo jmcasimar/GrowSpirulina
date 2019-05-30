f4c62f56ab0b34c317a87eb134fe983104fe308d
jmcasimar <chemanuel_95@hotmail.com>
2019-05-30 18:00:15 -0500

    Bug fixed: LED and waveMaker wrong behaivor

--- C++/arduinoMega/arduinoMega/otherFunctions.ino
+++ C++/arduinoMega/arduinoMega/otherFunctions.ino
@@ -2,36 +2,44 @@
 // Control pumps
 // Control LED´s
 void is_it_day(byte hr, byte mn){
-  if( hr >= dayBegin && hr < nightBegin && isItDay == LOW ){
-    isItDay = HIGH;
-    pumpOn = pumpOnDay;
-    pumpOff = pumpOffDay;
-    Serial.println(F("Starting day"));
+  if( hr >= dayBegin && hr < nightBegin){
+    if(isItDay == LOW){
+      isItDay = HIGH;
+      pumpOn = pumpOnDay;
+      pumpOff = pumpOffDay;
+      Serial.println(F("Starting day"));
+    }
   }
-  else if( (hr >= 23 || hr < 1) && isItDay == LOW ){
-    isItDay = HIGH;
-    pumpOn = pumpOnDay;
-    pumpOff = pumpOffDay;
-    Serial.println(F("Starting day"));
-    digitalWrite(led, !isItDay);
-    Serial.println(F("Turning on LED´s"));
+  else if( (hr >= 23 || hr < 1) ){
+    if(isItDay == LOW){
+      isItDay = HIGH;
+      pumpOn = pumpOnDay;
+      pumpOff = pumpOffDay;
+      Serial.println(F("Starting day"));
+      digitalWrite(led, !isItDay);
+      Serial.println(F("Turning on LED´s"));  
+    }
   }
-  else if( hr >= 3 && hr < 5 && isItDay == LOW ){
-    isItDay = HIGH;
-    pumpOn = pumpOnNight;
-    pumpOff = pumpOffNight;
-    Serial.println(F("Starting day"));
-    digitalWrite(led, !isItDay);
-    Serial.println(F("Turning on LED´s"));
+  else if( hr >= 3 && hr < 5){
+    if(isItDay == LOW){
+      isItDay = HIGH;
+      pumpOn = pumpOnNight;
+      pumpOff = pumpOffNight;
+      Serial.println(F("Starting day"));
+      digitalWrite(led, !isItDay);
+      Serial.println(F("Turning on LED´s"));
+    }
   }
-  else if( (hr >= nightBegin || hr < dayBegin) && isItDay == HIGH ){
-    isItDay = LOW;
-    pumpOn = pumpOnNight;
-    pumpOff = pumpOffNight;
-    Serial.println(F("Starting night"));
-    if(digitalRead(led)== !HIGH){
-      digitalWrite(led, !LOW);
-      Serial.println(F("Turning off LED´s"));
+  else if( (hr >= nightBegin || hr < dayBegin) ){
+    if(isItDay == HIGH){
+      isItDay = LOW;
+      pumpOn = pumpOnNight;
+      pumpOff = pumpOffNight;
+      Serial.println(F("Starting night"));
+      if(digitalRead(led)== !HIGH){
+        digitalWrite(led, !LOW);
+        Serial.println(F("Turning off LED´s"));
+      } 
     }
   }
 }
@@ -86,10 +94,12 @@ void getTemp_controlHR(){
 /***** Control waveMaker *****/
 void control_wM(byte hr, byte mn){
   // Turn on 10 minutes at 2, 9, 14, 19
-  if( ((hr == 9 && mn <= wM_On) || (hr == 14 && mn <= wM_On) || (hr == 19 && mn <= wM_On) || (hr == 2 && mn <= wM_On) ) && wM_State == LOW ){
-    wM_State = HIGH;  
-    digitalWrite(waveMaker, !wM_State);  
-    Serial.println(F("waveMaker turned on"));
+  if( ((hr == 9 && mn <= wM_On) || (hr == 14 && mn <= wM_On) || (hr == 19 && mn <= wM_On) || (hr == 2 && mn <= wM_On) ) ){
+    if(wM_State == LOW){
+      wM_State = HIGH;  
+      digitalWrite(waveMaker, !wM_State);  
+      Serial.println(F("waveMaker turned on")); 
+    }
   }
   else if(wM_State == HIGH){
     wM_State = LOW;
@@ -99,8 +109,8 @@ void control_wM(byte hr, byte mn){
 }
 
 void waterLevel_control(){
-  
-  if(digitalRead(waterLevel)==HIGH){ //Le puse LOW para que no de lata
+  if(digitalRead(waterLevel)==LOW){ // LOW mientras se conecta el sensor
+  //if(digitalRead(waterLevel)==HIGH){
     Serial.println("Water Level too high");
     delay(1000);
   }
